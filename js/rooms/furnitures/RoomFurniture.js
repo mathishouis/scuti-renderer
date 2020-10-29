e60b0b4b0c99c3042fda35419f9ecd1572229b78
HOUIS Mathis <mathishouis@gmail.com>
2020-10-30 00:46:40 +0100

    Starting furnitures!!! (jcontinuerais demain bonne nuit les loulous)

---
+++ js/rooms/furnitures/RoomFurniture.js
@@ -0,0 +1,8 @@
+export default class RoomFurniture {
+    constructor(id, position, direction, state) {
+        this.id = id;
+        this.position = position;
+        this.direction = direction;
+        this.state = state;
+    }
+}
