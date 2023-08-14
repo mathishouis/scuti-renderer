f5dc97804e801591b7cebf95c044dc51b3b47774
Mathis HOUIS <mathishouis@gmail.com>
2023-08-14 04:04:57 +0200

    + Created the ViteJS project

--- src/interfaces/Interaction.d.ts
+++
@@ -1,9 +0,0 @@
-import type { FederatedPointerEvent } from 'pixi.js';
-
-import type { IFloorPosition, IWallPosition } from './Furniture';
-
-export interface IInteractionEvent {
-  tag?: string;
-  mouseEvent: FederatedPointerEvent;
-  position?: IWallPosition | IFloorPosition;
-}
--- src/objects/rooms/parts/RoomPart.ts
+++
@@ -1,6 +0,0 @@
-import {Container} from "pixi.js";
-
-export abstract class RoomPart extends Container {
-  //abstract render(): void;
-  //abstract update(): void;
-}
