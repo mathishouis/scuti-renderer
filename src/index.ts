2efe92c5d33da421b9d1042450a113982b9bfc2c
Walid <ma.walidkorchi@icloud.com>
2023-08-20 04:14:26 +0100

    feat(bundler): implement esbuild (#58)
    * chore(deps): update latest
    
    * refactor(tsconfig): enforce type checking
    
    * feat(bundler): implement tsup along with esbuild
    
    * refactor(index): export necessary modules for external usage

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+export * from './Scuti'
+
+export * from './objects/rooms/Room'
+export * from './objects/rooms/RoomCamera'
+export * from './objects/rooms/RoomConfiguration'
+export * from './objects/rooms/RoomVisualization'
+export * from './objects/rooms/RoomEvents'
+export * from './objects/GameObject'
+export * from './objects/assets/AssetLoader'
+export * from './objects/events/EventManager'
---
+++ tsup.config.ts
@@ -0,0 +1,11 @@
+import type { Options } from 'tsup'
+
+export const tsup = {
+  target: 'esnext',
+  format: 'esm',
+  clean: true,
+  bundle: true,
+  minify: true,
+  entry: ['src/index.ts'],
+  dts: true
+} satisfies Options
